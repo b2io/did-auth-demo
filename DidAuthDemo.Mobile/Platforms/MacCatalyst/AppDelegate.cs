755021faa48d5026d8bb7cb0e152d19acf84da7b
Kyle Johns <kjohns1337@gmail.com>
2023-06-22 17:23:28 -0400

    started a mobile app for the holder

---
+++ DidAuthDemo.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DidAuthDemo.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
