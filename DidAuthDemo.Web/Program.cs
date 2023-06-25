a7f284b42f382d9549581a425987acda907995af
Kyle Johns <kjohns1337@gmail.com>
2023-06-24 21:47:41 -0400

    got the wallet connect working

--- DidAuthDemo.Web/Program.cs
+++ DidAuthDemo.Web/Program.cs
@@ -2,12 +2,18 @@ using Blazored.LocalStorage;
 using DidAuthDemo.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
