4a9ff43b8b8ff342dee906cb67d62135d498d7f0
Kyle Johns <kjohns1337@gmail.com>
2023-07-05 08:58:29 -0400

    finished first draft of web resolver

--- DidAuthDemo.Maui/Resolvers/WebResolver.cs
+++ DidAuthDemo.Maui/Resolvers/WebResolver.cs
@@ -1,5 +1,12 @@
-﻿using DidAuthDemo.Maui.Models;
+﻿using CardanoSharp.Wallet.Extensions.Models;
+using CardanoSharp.Wallet.Models.Keys;
+using DidAuthDemo.Maui.Models;
+using Kotlin.Contracts;
+using SimpleBase;
 using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using static Android.Icu.Text.RelativeDateTimeFormatter;
 
 namespace DidAuthDemo.Maui.Resolvers;
 
@@ -13,21 +20,35 @@ public class WebResolver : BaseResolver, IWebResolver
 {
     public override async Task<bool> VerifyDidDocument(Did did, string password)
     {
-        var key = UnlockKey(did.KeyId, password);
+        // Unlocked the PrivateKey using the provided password
+        var key = await UnlockKey(did.KeyId, password);
 
-        //get did document
+        // Grab the DID Document from the Website associated with DID
         using var client = new HttpClient();
 
         var didDomainUrl = $"{did.Domain}/.well-known/did.json";
-        var response = await client.GetFromJsonAsync<DidDocument>(didDomainUrl);
-
-        //couldn't successfully get document
-        if (!response.suc)
+        HttpResponseMessage res = await client.GetAsync(didDomainUrl);
+        DidDocument didDocument;
+        if (res.IsSuccessStatusCode)
+        {
+            didDocument = await res.Content.ReadFromJsonAsync<DidDocument>();
+        }
+        else
+        {
+            string msg = await res.Content.ReadAsStringAsync();
+            Console.WriteLine(msg);
             return false;
+        }
 
+        if (didDocument is null) return false;
 
-
-        //validate did document public key
-
+        // Validate DID Document using stored Private Key 
+        //    and Public Key in DID Document
+        var privateKey = JsonSerializer.Deserialize<PrivateKey>(key.PrivateKey);
+        var publicKeyObj = didDocument.PublicKeys.FirstOrDefault(x => x.Id == $"{did.Identifier}#key-1");
+        var publicKey = new PublicKey(Base58.Bitcoin.Decode(publicKeyObj.PublicKeyBase58), null);
+        var message = Encoding.UTF8.GetBytes("message");
+        var signature = privateKey.Sign(message);
+        return publicKey.Verify(message, signature);
     }
 }
